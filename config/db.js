f3b7ad665872d668db7128c3c4abece610903093
Victor Ukoha <81536363+VictorChukwudi@users.noreply.github.com>
2023-09-14 07:33:34 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.dbURI_LOCAL, {
+    const conn = await mongoose.connect(process.env.dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
